7d7407d9b2378ba19f78a974f749305a53eb85e6
Anish Tilloo <anishtilloo@gmail.com>
2024-01-24 23:54:37 +0530

    made db connection

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const db = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDB connected !! DB Host:: ${db.connection.host}`);
+  } catch (error) {
+    console.error("MONGODB connection FAILED: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
