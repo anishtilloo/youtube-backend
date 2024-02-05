3d1b5fb504acbfa027c3f8417133cf3d59d4e4a5
Anish Tilloo <anishtilloo@gmail.com>
2024-02-05 22:31:00 +0530

    basic configuration of cors and cookies are done

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+// This cookie-parser package is used to perform CRUD operations on the user cookies
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// this configures the the express to accept the json type data upto 16 kb which we have set as limit
+app.use(express.json({ limit: "16kb" }));
+
+// when we make a request data which is sent by params sent sent in encoded format and extended checks for the nested objects
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 // require("dotenv").config({ path: './env' });
 import dotenv from "dotenv";
-import connectDB  from "./db/index.js";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
-connectDB();
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error(
+        ` Error thrown::::ERROR during Listening to PORT !!!`,
+        error
+      );
+      process.exit(1);
+    });
+    app.listen(process.env.PORT || 9000, () => {
+      console.log(` Server running on port ${process.env.PORT || 9000}`);
+    });
+  })
+  .catch((error) => {
+    console.error(" Error thrown::::MONGO DB connection FAILED !!! ", error);
+  });
