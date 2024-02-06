f5770ada0ae902d45f42d87c41ed6223237c2759
Anish Tilloo <anishtilloo@gmail.com>
2024-02-06 23:20:25 +0530

    created user and video models and setup some middlewares, plugins, and methods in mogoDB using mongoose

---
+++ src/models/user.model.js
@@ -0,0 +1,104 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      // we add the index flag to the fields we want to make searchable
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      // we add the index flag to the fields we want to make searchable
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// pre is a middlerware in mongoDB which runs before the method we have mentions as string 
+//  here in this case it is save, whenever save method called this runs before the save is ex
+// we are using normal functions instead of arrow functions because in arrow functions does not have to the context using this
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+
+// these are the special methods defined to do certian tasks
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//  jwt sign method takes 
+//  paylode(a fancy word for the req data), 
+//  a token secret, 
+//  object in which the expiration time is given
+// generation access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generation refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+
+export const User = mongoose.model("User", userSchema);
