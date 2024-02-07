ee0ac45f7f20fd4dcb77b87597734b2ecc92cf17
Anish Tilloo <anishtilloo@gmail.com>
2024-02-08 00:22:48 +0530

    configured file uplode using multer and uploded on cloudinary service.

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,33 @@
+import path from "path";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    let ext = path.extname(file.originalname);
+    cb(null, Date.now() + "_" + ext);
+  },
+});
+
+const uplode = multer({
+  storage: storage,
+  //   fileFilter: function (req, file, callback) {
+  //     if (
+  //       file.mimetype == "image/png" ||
+  //       file.mimetype == "image/jpg" ||
+  //       file.mimetype == "image/jpeg"
+  //     ) {
+  //       callback(null, true);
+  //     } else {
+  //       console.log("Only jpg or png can be uploaded");
+  //       callback(null, false);
+  //     }
+  //   },
+  //   limits: {
+  //     fileSize: 1024 * 1024 * 2,
+  //   },
+});
+
+export default uplode;
