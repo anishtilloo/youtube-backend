f1104ea490a5934d2fca74b864ee5f8e81ed5b35
Anish Tilloo <anishtilloo@gmail.com>
2024-04-20 23:53:38 +0530

    added get all videos controller with pagenation

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,6 +9,33 @@ import { deleteVideosOnCloudinary, uploadOnCloudinary } from "../utils/cloudinar
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
+  const options = {
+    page: page,
+    limit: limit,
+    query: query,
+    sortBy: sortBy,
+    sortType: sortType,
+  };
+
+  try {
+      const videoAggregate = await Video.aggregate();
+      const resultWithPagenation = await Video.aggregatePaginate(videoAggregate, options);
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            { data: resultWithPagenation },
+            "Successfully fetched the videos"
+          )
+        );
+  } catch (error) {
+    return res
+      .status(500)
+      .json(
+        new ApiError(500, { error: error }, "Someting went wrong while fetching the videos")
+      );
+  }
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
