51d371b74d168049f2b8eb26b3ff133cd24aef02
Anish Tilloo <anish.tilloo@claimgenius.com>
2025-04-01 11:56:07 +0530

    corrected spelling for health check and dummy

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // routes
 //routes import
 import userRouter from "./routes/user.routes.js";
-import healthcheckRouter from "./routes/healthcheck.routes.js";
+import healthCheckRouter from "./routes/healthCheck.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import videoRouter from "./routes/video.routes.js";
@@ -35,7 +35,7 @@ import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/healthCheck", healthCheckRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
